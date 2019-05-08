cf0ff74e4a763a519730c5eff49b9a0b89746356
Jordan Babbitt <JBabbitt27@gmail.com>
2019-05-08 10:30:57 -0700

    heroku try 3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
